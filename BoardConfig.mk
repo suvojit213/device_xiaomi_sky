670262f2786f71199654b1997d2daad200b7c3ed
Lostark13 <tushar9915@gmail.com>
2024-04-28 22:51:13 +0000

    sky: setup libinit for different models
    Co-authored-by: SebaUbuntu <seba@sebaubuntu.dev>
    Signed-off-by: suvojit213 <suvojitsengupta21@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -92,6 +92,9 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_sky
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/configs/config.fs
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_sky
+
 # Kernel
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_BASE := 0x00000000
