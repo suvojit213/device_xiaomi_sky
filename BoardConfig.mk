449e8b8b2f82442cc9a4267be8e5f94c379de516
Lostark13 <tushar9915@gmail.com>
2024-05-12 14:18:27 +0300

    sky: Update from HyperOS V816.0.4.0.UMWINXM

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -220,7 +220,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 TARGET_SCREEN_DENSITY := 440
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2024-02-01
+VENDOR_SECURITY_PATCH := 2024-04-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
