575238451746046c188cefff8da407b5fe3fe2c3
Lostark13 <tushar9915@gmail.com>
2024-05-13 22:06:30 +0300

    Sky: Added Support For FM Radio

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -91,6 +91,9 @@ BOARD_PREBUILT_DTBOIMAGE := $(KERNEL_PATH)/dtbs/dtbo.img
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/configs/config.fs
 
+# FM
+BOARD_HAVE_QCOM_FM := true
+
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_sky
 
--- device.mk
+++ device.mk
@@ -178,6 +178,11 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
+	
+# FM
+PRODUCT_PACKAGES += \
+    FM2 \
+    qcom.fmradio
 
 # F2FS utilities
 PRODUCT_PACKAGES += \
