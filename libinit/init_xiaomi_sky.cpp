79f088bea46b45c90a0c2344590cc90643bdc1e1
Lostark13 <tushar9915@gmail.com>
2024-06-01 21:25:24 +0200

    sky: Add support for NFC devices
    Signed-off-by: suvojit213 <suvojitsengupta21@gmail.com>

--- libinit/init_xiaomi_sky.cpp
+++ libinit/init_xiaomi_sky.cpp
@@ -9,9 +9,10 @@
 
 #include "vendor_init.h"
 
-#define FINGERPRINT_POCO_IN "POCO/sky:13/TKQ1.221114.001/V14.0.5.0.TMWINXM:user/release-keys"
-#define FINGERPRINT_REDMI_IN "Redmi/sky:13/TKQ1.221114.001/V14.0.5.0.TMWINXM:user/release-keys"
-#define FINGERPRINT_CHINA "Redmi/sky/sky:13/TKQ1.221114.001/V14.0.5.0.TMWINXM:user/release-keys"
+#define FINGERPRINT_POCO_IN "POCO/sky_p_in/sky:14/UKQ1.231003.002/V816.0.2.0.UMWINXM:user/release-keys"
+#define FINGERPRINT_REDMI_IN "Redmi/sky_in/sky:14/UKQ1.231003.002/V816.0.2.0.UMWINXM:user/release-keys"
+#define FINGERPRINT_CHINA "Redmi/sky/sky:14/UKQ1.231003.002/V816.0.5.0.UMWCNXM:user/release-keys"
+#define FINGERPRINT_GLOBAL_NFC "Redmi/river_global/river:14/UKQ1.231003.002/V816.0.4.0.UMWMIXM:user/release-keys"
 
 static const variant_info_t sky_poco_info = {
     .hwc_value = "India",
@@ -21,7 +22,7 @@ static const variant_info_t sky_poco_info = {
     .device = "sky",
     .marketname = "POCO M6 Pro 5G",
     .model = "23076PC4BI",
-    .mod_device = "sky_in_global",
+    .mod_device = "sky_global",
     .build_fingerprint = FINGERPRINT_POCO_IN,
 };
 
@@ -33,26 +34,103 @@ static const variant_info_t sky_redmi_info = {
     .device = "sky",
     .marketname = "Redmi 12 5G",
     .model = "23076RN4BI",
-    .mod_device = "sky_in_global",
+    .mod_device = "sky_global",
     .build_fingerprint = FINGERPRINT_REDMI_IN,
 };
 
+static const variant_info_t sky_redmi2_info = {
+    .hwc_value = "India",
+    .boardid = "S88019EA1",
+
+    .brand = "Redmi",
+    .device = "sky",
+    .marketname = "Redmi 12 5G",
+    .model = "23076RN4BI",
+    .mod_device = "sky_global",
+    .build_fingerprint = FINGERPRINT_REDMI_IN,
+};
 static const variant_info_t sky_china_info = {
     .hwc_value = "CN",
     .boardid = "S88019BA1",
 
     .brand = "Redmi",
-    .device = "sky",
+    .device = "river",
     .marketname = "Redmi Note 12R",
     .model = "23076RA4BC",
     .mod_device = "sky_global",
     .build_fingerprint = FINGERPRINT_CHINA,
 };
 
+static const variant_info_t sky2_china_info = {
+    .hwc_value = "CN",
+    .boardid = "S88019OA1",
+
+    .brand = "Redmi",
+    .device = "sky",
+    .marketname = "Redmi 12R",
+    .model = "23076RA4BC",
+    .mod_device = "sky_global",
+    .build_fingerprint = FINGERPRINT_REDMI_IN,
+};
+
+static const variant_info_t river_info_S88029AA1 = {
+    .hwc_value = "Global",
+    .boardid = "S88029AA1",
+
+    .brand = "Redmi",
+    .device = "river",
+    .marketname = "Redmi 12 5G",
+    .model = "23076RN8DY",
+    .mod_device = "sky_global",
+    .build_fingerprint = FINGERPRINT_GLOBAL_NFC,
+};
+
+static const variant_info_t river_info_S88019BP1 = {
+    .hwc_value = "CN",
+    .boardid = "S88019BP1",
+
+    .brand = "Redmi",
+    .device = "river",
+    .marketname = "Redmi 12 5G",
+    .model = "23077RABDC",
+    .mod_device = "sky_global",
+    .build_fingerprint = FINGERPRINT_GLOBAL_NFC,
+};
+
+static const variant_info_t river_info_S88018PA1 = {
+    .hwc_value = "Global",
+    .boardid = "S88018PA1",
+
+    .brand = "Redmi",
+    .device = "river",
+    .marketname = "Redmi 12 5G",
+    .model = "23077RABDC",
+    .mod_device = "sky_global",
+    .build_fingerprint = FINGERPRINT_GLOBAL_NFC,
+};
+
+static const variant_info_t river_info_S88029JA1 = {
+    .hwc_value = "Japan",
+    .boardid = "S88029JA1",
+
+    .brand = "Redmi",
+    .device = "river",
+    .marketname = "Redmi 12 5G",
+    .model = "23076RA4BR",
+    .mod_device = "sky_global",
+    .build_fingerprint = FINGERPRINT_GLOBAL_NFC,
+};
+
 static const std::vector<variant_info_t> variants = {
     sky_poco_info,
     sky_redmi_info,
+    sky_redmi2_info,
     sky_china_info,
+    sky2_china_info,
+    river_info_S88029AA1,
+    river_info_S88019BP1,
+    river_info_S88018PA1,
+    river_info_S88029JA1,
 };
 
 void vendor_load_properties() {
