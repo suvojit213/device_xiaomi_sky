9cf75670e3f4aab5d3e813c80e7e97b1e46932bf
suvojit213 <suvojitsengupta21@gmail.com>
2024-06-09 08:40:39 +0200

    sky: Bring-up For Pixelstar
    Signed-off-by: suvojit213 <suvojitsengupta21@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_sky.mk
+    $(LOCAL_DIR)/pixelstar_sky.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_sky-eng \
-    lineage_sky-userdebug \
-    lineage_sky-user
+    pixelstar_sky-eng \
+    pixelstar_sky-userdebug \
+    pixelstar_sky-user
