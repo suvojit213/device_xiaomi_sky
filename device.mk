cd1b01eebdb5b9488ce028800c5301359b87fd07
Lostark13 <tushar9915@gmail.com>
2024-06-14 18:51:57 +0000

    sky: Switch to common qti health aidl

--- device.mk
+++ device.mk
@@ -238,10 +238,8 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health-service.xiaomi \
-    android.hardware.health-service.xiaomi_recovery \
-    android.hardware.health@1.0.vendor \
-    android.hardware.health@2.1.vendor
+    android.hardware.health-service.qti \
+    android.hardware.health-service.qti_recovery
 
 # HIDL
 PRODUCT_PACKAGES += \
