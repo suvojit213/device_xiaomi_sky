fcf6c0a35c9b251675a8733d60a98114c28f2046
Arian <arian.kulmer@web.de>
2024-07-16 20:00:33 +0000

    sky: Build qti thermal hal from source
    Change-Id: Iff18a5fb4a5717aa6b77fdd64a87367dccd5df0b

--- device.mk
+++ device.mk
@@ -556,8 +556,7 @@ PRODUCT_COPY_FILES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.qti-v2 \
-    android.hardware.thermal@2.0.vendor
+    android.hardware.thermal@2.0-service.qti-v2
 
 PRODUCT_VENDOR_PROPERTIES += \
     vendor.sys.thermal.data.path=/data/vendor/thermal/
