a428ff575e3f08497d1e559b0a9e55e73037ca3b
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-09-21 23:09:54 +0000

    sky: Switch to 64-bit-only builds Remove 32bit support entirely from the OS.
    Co-authored-by: redducc <sushmit718@gmail.com>
    Change-Id: I705e2fca23dbdee297afcde45e9090b6d124e0be

--- lineage_sky.mk
+++ lineage_sky.mk
@@ -7,7 +7,7 @@
 DEVICE_PATH := device/xiaomi/sky
 
 # Configure core_64_bit.mk
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 
 # Configure full_base_telephony.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
