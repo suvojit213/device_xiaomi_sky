b5715f98a28ab4ac9be101228ab5051df59597b6
Lostark13 <tushar9915@gmail.com>
2024-09-21 16:46:14 +0000

    sky: Fixup ims libs symlink
    Change-Id: I08835617e221e3947ca72c471c9b4e59b71687d7

--- Android.mk
+++ Android.mk
@@ -39,7 +39,7 @@ ALL_DEFAULT_INSTALLED_MODULES += \
 
 # Symlinks
 IMS_LIBS := libimscamera_jni.so libimsmedia_jni.so
-IMS_SYMLINKS := $(addprefix $(TARGET_OUT_SYSTEM_EXT_APPS_PRIVILEGED)/Ims/lib/arm64/,$(notdir $(IMS_LIBS)))
+IMS_SYMLINKS := $(addprefix $(TARGET_OUT_SYSTEM_EXT_APPS_PRIVILEGED)/ims/lib/arm64/,$(notdir $(IMS_LIBS)))
 $(IMS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "IMS lib link: $@"
 	@mkdir -p $(dir $@)
