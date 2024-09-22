06dad4211a6a50a1757f826b04d8ff7c7a011ec9
Manish Kushwaha <kushwahamanish@google.com>
2024-09-22 00:49:10 +0000

    sky: Migrate sensor HAL to AIDL interface
    Bug: 247478991
    Test: tested on-device (P23) ab/P46941077
    Test: `sensor_test -ahal list` and `sensor_test list`
    Test: `sensor_test -ahal sample -s0` and `sensor_test sample -s0`
    
    Change-Id: I40f6ab6b7780c53cd638a1f77c95f6c324dc2d15
    Signed-off-by: ralph950412 <ralph950412@gmail.com>
    Signed-off-by: Rasenkai <rasenkai99@gmail.com>
    
    Change-Id: I9acf2be7f6e5fed2e5f82948c0b77f9d67783318

--- device.mk
+++ device.mk
@@ -484,7 +484,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0 \
     android.frameworks.sensorservice@1.0.vendor \
-    android.hardware.sensors@2.1-service.xiaomi-multihal \
+    android.hardware.sensors-service.xiaomi-multihal \
     libsensorndkbridge
 
 PRODUCT_COPY_FILES += \
