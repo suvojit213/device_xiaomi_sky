875f6be7e03ee05c772e62ec849405b202366d88
suvojit213 Name <suvojitsengupta21@gmail.com>
2024-09-22 00:29:58 +0000

    sky: Update blobs from V816.0.6.0.UMWINXM

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -218,7 +218,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 TARGET_SCREEN_DENSITY := 440
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2024-06-01
+VENDOR_SECURITY_PATCH := 2024-08-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
