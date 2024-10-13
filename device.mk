ddf1e0a41e876f0f7db1cfb87f40886cb6623d25
Bruno Martins <bgcngm@gmail.com>
2024-10-13 21:20:30 +0530

    sky: Build opensource soundtrigger HAL
    Change-Id: I956d53a67841a9e1e296de6b73f08ef2efac7470

--- device.mk
+++ device.mk
@@ -84,7 +84,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     audio.bluetooth.default:64 \
     audio.r_submix.default:64 \
-    audio.usb.default:64
+    audio.usb.default:64 \
+    sound_trigger.primary.parrot:64
 
 PRODUCT_PACKAGES += \
     audioadsprpcd
