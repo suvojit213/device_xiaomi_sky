9c2957d259b668d630a19dbdf0bb3fe990a788a0
Lostark13 <tushar9915@gmail.com>
2024-10-13 22:02:12 +0530

    sky: Cleanup manifest
    Change-Id: I9b754f64a933903ac3bc3ef4a96b2efdba811cef

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -253,8 +253,7 @@ BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX_LOCATION := 2
 DEVICE_MANIFEST_SKUS := ravelin
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
 DEVICE_MANIFEST_RAVELIN_FILES := \
-    $(DEVICE_PATH)/configs/vintf/manifest_ravelin.xml \
-    $(DEVICE_PATH)/configs/vintf/manifest_xiaomi.xml
+    $(DEVICE_PATH)/configs/vintf/manifest_ravelin.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/configs/vintf/vendor_framework_compatibility_matrix.xml \
     $(DEVICE_PATH)/configs/vintf/xiaomi_framework_compatibility_matrix.xml \
